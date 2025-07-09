64237eb2b14bbeeec412bff5d85878a4cc757afd
林夕 <1595818766@qq.com>
2025-07-09 16:05:31 +0800

    常量抽取

--- src/middleware/file.middleware.js
+++ src/middleware/file.middleware.js
@@ -1,6 +1,7 @@
 const multer = require("@koa/multer");
+const { UPLOAD_PATH } = require("../config/path");
 
-const uploadAvatar = multer({ dest: './uploads' });
+const uploadAvatar = multer({ dest: UPLOAD_PATH });
 
 const handleAvatar = uploadAvatar.single('avatar');
 
